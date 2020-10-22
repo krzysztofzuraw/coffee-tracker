e0f895d122fcc822074fd85c82762f733188d880
Krzysztof Żuraw <services@kzuraw.com>
2020-10-22 19:27:53 +0200

    feat: tweak layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,15 +4,17 @@ import styles from './layout.module.css';
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
   return (
-    <div className={styles.content}>
-      {children}
-      <footer>
-        <ul className={styles.nav}>
-          <li>Record</li>
-          <li>List</li>
-          <li>Settings</li>
-        </ul>
-      </footer>
+    <div className={styles.wrapper}>
+      <main className={styles.content}>
+        {children}
+        <footer>
+          <ul className={styles.nav}>
+            <li>Record</li>
+            <li>List</li>
+            <li>Settings</li>
+          </ul>
+        </footer>
+      </main>
     </div>
   );
 }
