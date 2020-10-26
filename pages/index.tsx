e1d211d9e690f0903d5eebc01c97ce55c4999187
Krzysztof Żuraw <services@kzuraw.com>
2020-10-26 19:24:01 +0100

    feat: add basic indexdb

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { openDB } from 'idb';
 
 import Layout from '../components/layout';
 import styles from '../styles/index.module.css';
 
+async function doDatabaseStuff() {
+  const db = await openDB('coffeeTracker', 1, {
+    upgrade(db) {
+      const store = db.createObjectStore('logs', {
+        keyPath: 'id',
+        autoIncrement: true,
+      });
+      store.createIndex('date', 'date');
+    },
+  });
+
+  await db.add('logs', { coffee: 'Etiopia', date: Date.now() });
+}
+
 export default function Index() {
+  useEffect(() => {
+    doDatabaseStuff();
+  }, []);
   return (
     <Layout>
       <form className={`${styles.form} stack`}>
