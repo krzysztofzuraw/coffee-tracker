49c16742e164f2986427e685cc251d61634e2912
Krzysztof Żuraw <services@kzuraw.com>
2020-11-11 13:44:34 +0100

    chore: add sentry support

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Coffee tracker
+
+## Default values for `.env.local`
+
+```
+NEXT_PUBLIC_SENTRY_DSN=
+# Only required to upload sourcemaps
+SENTRY_ORG=
+SENTRY_PROJECT=
+SENTRY_AUTH_TOKEN=
+```
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,16 @@ import React from 'react';
 
 import '../styles/reset.css';
 import '../styles/global.css';
+import { init } from '../helpers/sentry';
+import { NextPageContext } from 'next';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+init();
+
+export default function App({
+  Component,
+  pageProps,
+  err,
+}: AppProps & { err: NextPageContext['err'] }) {
+  // Workaround for https://github.com/vercel/next.js/issues/8592
+  return <Component {...pageProps} err={err} />;
 }
