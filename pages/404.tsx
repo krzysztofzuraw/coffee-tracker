62b23169d434324f93f5d612be41a70eb5f72f69
Krzysztof Żuraw <services@kzuraw.com>
2020-11-12 18:22:36 +0100

    chore: remove sentry

--- pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import Error from 'next/error';
-import React from 'react';
-
-export default function NotFound() {
-  // Do not record an exception in Sentry for 404
-  return <Error statusCode={404} />;
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,7 @@ import React from 'react';
 
 import '../styles/reset.css';
 import '../styles/global.css';
-import { init } from '../helpers/sentry';
-import { NextPageContext } from 'next';
 
-init();
-
-export default function App({
-  Component,
-  pageProps,
-  err,
-}: AppProps & { err: NextPageContext['err'] }) {
-  // Workaround for https://github.com/vercel/next.js/issues/8592
-  return <Component {...pageProps} err={err} />;
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
